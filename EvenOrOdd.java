3df0d1b6d3ba715efcccaa138919885377cf539e
julinbestina <julin.bestina@yahoo.com>
2022-05-31 12:35:07 +0530

    Added code to check Even or Odd

---
+++ EvenOrOdd.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.day5;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
+
+        if( number % 2 == 0){
+            System.out.println(number + " is a Even Number");
+        }
+        else {
+            System.out.println(number + " is not a Even Number");
+        }
+    }
+}
